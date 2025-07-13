238a85a0580c98a0b2f647a7785fa724d6bcc6d1
kekee000 <kekee000@gmail.com>
2025-07-14 01:03:14 +0800

    feat: support connect

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 cd $(dirname $0)
 [ -d ./dist ] && rm -r dist
+nodeVersion=$(node -p "process.version.match(/v(\d+)\./)[1]")
 
-npm run prod
+if [ $nodeVersion -lt 17 ]; then
+    npm run prod:node-old-version
+else
+    npm run prod
+fi
+
 
 cp -r dep dist
 cp -r font dist
