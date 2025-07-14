4c9182dca1846ea07135a4633a7e15cdc8c0d447
kekee000 <kekee000@gmail.com>
2025-07-14 15:41:27 +0800

    feat: support figma connect

--- src/fonteditor/connect.js
+++ src/fonteditor/connect.js
@@ -5,31 +5,51 @@ async function saveProject(fontFamily, ttf) {
     const id = await project.add(
         fontFamily,
         ttf,
-        options.config
+        {}
     );
     return id;
 }
 
 function main() {
+    if (parent !== window) {
+        parent.postMessage({ type: 'loaded', data: {} }, '*');
+    }
+    else if (window.opener) {
+        window.opener.postMessage({ type: 'loaded', data: {} }, '*');
+    }
+
     window.addEventListener('message', function (event) {
         if (event.data && event.data.type === 'create-font') {
             const config = event.data.data || {};
             let ttf = null;
-            if (config.ttf) {
-                ttf = config.ttf;
+            if (config.ttfObject) {
+                ttf = config.ttfObject;
+                console.info('Received ttfObject from parent:', event.data.type);
             }
             if (!ttf) {
                 return;
             }
-            saveProject(config.fontFamily, ttf).then(function (id) {
-                console.info('Project created with ID:', id);
-                const lang = (window.navigator.language || 'zh-cn').toLowerCase();
-                const baseUrl = window.location.href.slice(0, window.location.href.lastIndexOf('/'));
-                const projectUrl = lang === 'zh-cn' ? `${baseUrl}/index.html?project=${id}` : `${baseUrl}/index-en.html?project=${id}`;
-                location.replace(projectUrl);
+            const baseUrl = window.location.href.slice(0, window.location.href.lastIndexOf('/'));
+            const lang = (window.navigator.language || 'zh-cn').toLowerCase();
+
+            saveProject(config.fontFamily, ttf).then((projectId) => {
+                console.info('Project created with ID:', projectId);
+                const projectUrl = `${baseUrl}/${lang === 'zh-cn' ? 'index' : 'index-en'}.html?project=${projectId}`;
+                if (parent !== window) {
+                    parent.postMessage({ type: 'create-font-result', data: {success: true, projectId, projectUrl} }, '*');
+                }
+                else {
+                    location.replace(projectUrl);
+                }
             })
             .catch(e => {
                 console.error('Error creating project:', e);
+                if (parent !== window) {
+                    parent.postMessage({ type: 'creat-font-result', data: {success: false, error: e.message } }, '*');
+                }
+                else {
+                    location.replace(`${baseUrl}/${lang === 'zh-cn' ? 'index' : 'index-en'}.html`);
+                }
             });
         }
     });
